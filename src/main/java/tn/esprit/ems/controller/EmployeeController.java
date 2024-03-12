02080abb525ef2de4a21fb04791022e116cd4184
ChihebDabebi <dabebi.chiheb@esprit.tn>
2024-03-12 15:52:39 +0100

    add method in the controller

---
+++ src/main/java/tn/esprit/ems/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package tn.esprit.ems.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tn.esprit.ems.dto.EmployeeDto;
+import tn.esprit.ems.entity.Employee;
+import tn.esprit.ems.service.EmployeeService;
+import tn.esprit.ems.service.impl.EmployeeServiceImpl;
+
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
