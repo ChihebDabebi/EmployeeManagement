7c7859f06d750bb57bf4f4868b9395459970378a
ChihebDabebi <dabebi.chiheb@esprit.tn>
2024-03-12 13:23:37 +0100

    first commit

---
+++ src/test/java/tn/esprit/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
