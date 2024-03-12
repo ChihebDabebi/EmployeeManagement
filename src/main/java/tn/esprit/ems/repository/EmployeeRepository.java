1cf8efeefb8e2b217f8ef90efbe1799b7b9d6f21
ChihebDabebi <dabebi.chiheb@esprit.tn>
2024-03-12 14:12:46 +0100

    Employee Repository that implements JpaRepository

---
+++ src/main/java/tn/esprit/ems/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package tn.esprit.ems.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tn.esprit.ems.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+ 
+}
